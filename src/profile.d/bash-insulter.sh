12ea357d0f2fca98403f10ad94caba56b653c14b
Kristopher Gamrat <gamrat.kristopher@gmail.com>
2017-12-22 10:44:50 -0500

    Edits to src/profile.d/bash-insulter.sh:  - Change variable GLOBAL to GLOBAL_BASH_INSULTER to fix working with BASH.  - Add logic to echo an error message if GLOBAL_BASH_INSULTER is unset.

--- src/profile.d/bash-insulter.sh
+++ src/profile.d/bash-insulter.sh
@@ -1,10 +1,15 @@
 # Whether or not to globally enable bash-insulter
 # 0 = disabled
 # anything else = enabled
-GLOBAL=1
+GLOBAL_BASH_INSULTER=0
 
-if [ "${GLOBAL}" -ne 0 ]
-then
+if [ -z "${GLOBAL_BASH_INSULTER}" ]; then
+	echo "GLOBAL_BASH_INSULTER unset. Please edit /etc/profile.d/bash-insulter.sh"
+	echo "and include one of the following at the top of the file:"
+	echo ""
+	echo "GLOBAL_BASH_INSULTER=0"
+	echo "GLOBAL_BASH_INSULTER=1"
+elif [ "${GLOBAL_BASH_INSULTER}" -ne 0 ]; then
 	[ -f /etc/bash.command-not-found-messages ] && . /etc/bash.command-not-found-messages
 	[ -f /etc/bash.command-not-found ] && . /etc/bash.command-not-found
 fi
