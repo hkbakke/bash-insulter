d4b6abf7810cf9bac6b4faaf82b8db8e147f2131
Kristopher Gamrat <gamrat.kristopher@gmail.com>
2017-12-22 10:12:57 -0500

    Add logic to enable/disable bash-insulter globally

--- src/profile.d/bash-insulter.sh
+++ src/profile.d/bash-insulter.sh
@@ -1,3 +1,10 @@
-[ -f /etc/bash.command-not-found-messages ] && . /etc/bash.command-not-found-messages
+# Whether or not to globally enable bash-insulter
+# 0 = disabled
+# anything else = enabled
+GLOBAL=1
 
-[ -f /etc/bash.command-not-found ] && . /etc/bash.command-not-found
+if [ "${GLOBAL}" -ne 0 ]
+then
+	[ -f /etc/bash.command-not-found-messages ] && . /etc/bash.command-not-found-messages
+	[ -f /etc/bash.command-not-found ] && . /etc/bash.command-not-found
+fi
